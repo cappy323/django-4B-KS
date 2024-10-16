fab3483d78b0253a0fdca09cb9c622e8ea46e645
Kacper Szreder <kacpiszreder@gmail.com>
2024-10-16 08:27:22 +0200

    git add

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
