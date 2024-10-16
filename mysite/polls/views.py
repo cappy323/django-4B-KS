0a1f784be4b2de9dcc21a27c4cbd0230e17e40d4
Kacper Szreder <kacpiszreder@gmail.com>
2024-10-16 08:22:51 +0200

    git add

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,15 +4,19 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
-
-
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
